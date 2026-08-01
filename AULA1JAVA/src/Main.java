ae4cf67dfbecc02a9ca0696dc1b4ba18186d8182
Luann Rangel <luannrangel@gmail.com>
2026-08-01 11:24:48 -0300

    teste

--- AULA1JAVA/src/Main.java
+++ AULA1JAVA/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("Hello JAVA, 1° AULA de Java - 2° semestre de 2026");
         int x = 10;
-        int y = 100;
+        int y = 50;
         float saida = x*y;
 
         System.out.println("A multiplicação de x por y é igual a :"+ (saida));
